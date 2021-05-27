4e8b3b39004650295b9f34272f0583274de21982
affinity96 <affinity96@gmail.com>
2021-05-27 20:29:15 +0900

    update 0527

---
+++ BaekJoon/210527/15650.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#define MAX 100
+using namespace std;
+int N, M;
+vector <int> nums;
+vector <bool> visited;
+int arr[MAX];
+void dfs(int num, int cnt){
+    
+    if(cnt == M){
+        for(int i=0; i<M; i++){
+            cout << arr[i] << " ";
+
+        }
+        cout << "\n";
+        return;
+    }else{
+        for(int i=num; i<=N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[cnt] = i;
+                dfs(i+1, cnt+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
+int main(){
+    cin >> N >> M;
+
+    nums.assign(N, 0);
+    for(int n=0; n<N; n++){
+        nums[n] = n+1;
+    }
+    visited.assign(N+1, false);
+    dfs(1, 0);
+}
